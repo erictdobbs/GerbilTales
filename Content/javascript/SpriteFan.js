396467c55f3129a641f995caaff2dd5ddd78dd29
erictdobbs <erictdobbs@gmail.com>
2015-12-29 22:56:07 -0500

    Sync fix

---
+++ Content/javascript/SpriteFan.js
@@ -0,0 +1,77 @@
+﻿function Fan(x, y, width, height, powerSource) {
+    SpriteBase.call(this, x + width/2, y + height/2);
+    this.width = width;
+    this.height = height;
+    this.range = 300;
+
+    this.powerSource = powerSource;
+
+    this.fanParticles = []
+    for (var i = 0; i < 9; i++) this.fanParticles.push(new FanParticle(this, Math.random() * this.width, 16 * i));
+    
+    this.color = new Color(100, 128, 128, 1.0);
+    this.borderColor = new Color(80, 80, 80, 1.0);
+
+    this.executeRules = function () {
+        for (var i = 0; i < sprites.length; i++) {
+            if (sprites[i] instanceof Gerbil) {
+                if (sprites[i].getBottom() <= this.getTop() &&
+                    sprites[i].getRight() > this.getLeft() &&
+                    sprites[i].getLeft() < this.getRight()) {
+                    sprites[i].dy -= this.getFanStrength(sprites[i].y);
+                    sprites[i].dy -= Math.random() * 0.2;
+                    if (sprites[i].getBottom() == this.getTop()) sprites[i].y -= 1;
+                }
+            }
+        }
+    };
+
+    this.getPower = function () {
+        if (this.powerSource) return this.powerSource.power;
+        return 0;
+    }
+
+    this.getFanStrength = function(y) {
+        return (this.range - (this.y - y)) / this.range * this.getPower() / 2;
+    }
+
+    this.draw = function () {
+        gameViewContext.fillStyle = this.color.toString();
+        gameViewContext.fillRect(this.getLeft(), this.getTop(), this.width, this.height);
+        gameViewContext.strokeStyle = this.borderColor.toString();
+        gameViewContext.lineWidth = 3;
+        gameViewContext.strokeRect(this.getLeft(), this.getTop(), this.width, this.height);
+
+        gameViewContext.font = "20px monospace";
+        gameViewContext.fillStyle = this.borderColor.toString();
+        gameViewContext.fillText(sprites.indexOf(this), this.x - 11, this.y + 5);
+
+        for (var i = 0; i < this.fanParticles.length; i++) this.fanParticles[i].draw();
+    }
+}
+Fan.prototype = new SpriteBase();
+Fan.prototype.constructor = Fan;
+
+function FanParticle(fan, x, y) {
+    this.fan = fan;
+    this.x = x;
+    this.y = y;
+    this.dy = 0;
+
+    this.draw = function () {
+        var fanStrength = this.fan.getFanStrength(this.y);
+        this.dy = (10 * fanStrength) - 1;
+        this.y -= this.dy;
+        if (this.y > this.fan.range / 2) {
+            this.y = 0;
+            this.x = this.fan.width * Math.random();
+        }
+
+        var fanColor = this.fan.borderColor;
+        var alpha = Math.pow(this.fan.getPower() * (this.fan.range - this.y) / this.fan.range, 0.4);
+        if (alpha > 1.0) alpha = 1.0;
+        this.color = new Color(fanColor.r, fanColor.g, fanColor.b, alpha);
+        gameViewContext.strokeStyle = this.color.toString();
+        gameViewContext.strokeRect(this.fan.getLeft() + this.x, this.fan.getTop() - this.y, 1, 1);
+    }
+}
---
+++ Content/javascript/SpriteWheel.js
@@ -0,0 +1,87 @@
+﻿function Wheel(x, y, diameter) {
+    SpriteBase.call(this, x, y);
+    this.width = diameter;
+    this.height = diameter;
+    this.maxRunners = 3;
+    this.runners = [];
+    this.power = 0;
+    
+    this.color = new Color(192, 192, 128, 0.7);
+    this.borderColor = new Color(100, 100, 80, 1.0);
+    this.isClicked = false;
+
+    this.executeRules = function () {
+        if (!this.isClicked && keyboardState.isKeyPressed(keyboardState.key.S) && this.runners.length > 0) {
+            this.isClicked = true;
+            this.runners[0].wheel = null;
+            this.runners[0].setRight(this.getLeft() - 1);
+            this.runners[0].dx = -3;
+            this.runners.splice(0, 1);
+        }
+        if (!keyboardState.isKeyPressed(keyboardState.key.S)) {
+            this.isClicked = false;
+        }
+
+        for (var i = 0; i < this.runners.length; i++) this.updateRunner(this.runners[i]);
+        this.power += this.runners.length * 0.0003;
+        if (this.power > this.runners.length / this.maxRunners) this.power = this.runners.length / this.maxRunners;
+        for (var i = 0; i < sprites.length; i++) {
+            if (sprites[i] instanceof Gerbil && this.doesOverlapSprite(sprites[i])) {
+                if (sprites[i].wheel == null && this.runners.length < this.maxRunners) {
+                    this.runners.push(sprites[i]);
+                    sprites[i].x = this.x + (Math.random() - 0.5) * this.width / 3;
+                    sprites[i].y = this.y;
+                    sprites[i].dx = 0.2;
+                    sprites[i].dy = 0;
+                    sprites[i].wheel = this;
+                    return;
+                }
+            }
+        }
+    };
+
+    this.updateRunner = function (runner) {
+        runner.x += runner.dx;
+        runner.y += runner.dy;
+        var radius = this.width / 2 - 8;
+        var minX = this.x - this.width / 3;
+        if (runner.x < minX) {
+            runner.x += 2;
+            runner.y -= 2;
+            runner.dx = 2 + Math.random() * 3;
+            runner.dy = -3;
+        }
+        var maxX = this.x + radius;
+        if (runner.x > maxX) runner.x = maxX;
+        var maxY = this.y + Math.pow(-Math.pow(this.x - runner.x, 2) + Math.pow(radius, 2), 0.5);
+        if (runner.y > maxY) {
+            runner.y = maxY;
+            runner.dy = 0;
+            runner.dx = -5 * this.power * (runner.y - this.y) / radius;
+        }
+        var maxY = this.y + radius;
+        if (runner.y > maxY) runner.y = maxY;
+        runner.dy += 0.4;
+    }
+
+    this.draw = function () {
+        gameViewContext.fillStyle = this.color.toString();
+        gameViewContext.strokeStyle = this.borderColor.toString();
+        gameViewContext.beginPath();
+        gameViewContext.arc(this.x, this.y, this.width/2, 0, 2 * Math.PI);
+        //gameViewContext.fill();
+        gameViewContext.stroke();
+
+        //gameViewContext.fillStyle = this.color.toString();
+        //gameViewContext.fillRect(this.getLeft(), this.getTop(), this.width, this.height);
+        //gameViewContext.strokeStyle = this.borderColor.toString();
+        //gameViewContext.lineWidth = 3;
+        //gameViewContext.strokeRect(this.getLeft(), this.getTop(), this.width, this.height);
+
+        gameViewContext.font = "20px monospace";
+        gameViewContext.fillStyle = this.borderColor.toString();
+        gameViewContext.fillText(sprites.indexOf(this), this.x - 11, this.y + 5);
+    }
+}
+Wheel.prototype = new SpriteBase();
+Wheel.prototype.constructor = Wheel;
